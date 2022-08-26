a9897249523a99e39f3aeb2c64d13a978400ac85
Darshan Mahajan <darshan@dotminds.in>
2022-08-26 19:43:02 +0530

    initial commit

---
+++ rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { Text,View,StyleSheet } from "react-native";
+
+
+const ListScreen = () => {
+    return <Text>This is List Screen</Text>
+}
+
+
+const styles = StyleSheet.create({}); 
+
+export default ListScreen;
